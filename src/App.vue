1f9bd3137e0bb2dd43f05f75c3936f3bbb0f4db6
Дмитрий Халин <dimaskhln@gmail.com>
2021-02-18 19:23:39 +0700

    First commit

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center"></div>
-      <span>MEGACALCULATOR</span>
+      <router-link to="/calculator">/calculator</router-link>
+      <router-link to="/base-to-base"><v-btn>BASE TO BASE</v-btn></router-link>
+      <router-link to="/polinom"><v-btn>POLINOM</v-btn></router-link>
+      <v-btn>CALCULATOR</v-btn>
       <v-spacer></v-spacer>
+      <v-btn>CALCULATOR</v-btn>
     </v-app-bar>
 
     <v-main>
-      <Calculator />
+      <router-view name="mainView"></router-view>
     </v-main>
   </v-app>
 </template>
@@ -18,9 +22,14 @@ import Calculator from './components/Calculator';
 export default {
   name: 'App',
 
-  components: {
-    Calculator
-  },
+  routes: [
+    {
+      path: '/calculator',
+      components: {
+        mainView: Calculator
+      }
+    }
+  ],
 
   data: () => ({
     //
