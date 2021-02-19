89a86f41f99b730272f1f5c8e19028a811c760b2
dimaskhln <dimaskhln@gmail.com>
2021-02-19 19:33:45 +0700

    Changed navigation bar. Russification. Base to base working well

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer fixed color="blue darken-1" padless>
     <v-row justify="center" no-gutters>
-      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12"> {{ new Date().getFullYear() }} — <strong>Dmitriy Khalin</strong> </v-col>
+      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12"> {{ new Date().getFullYear() }} — <strong>Дмитрий Халин</strong> </v-col>
     </v-row>
   </v-footer>
 </template>
---
+++ src/components/TabNavigator.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card class="mx-3 mt-3">
+    <v-tabs background-color="blue darken-1" center-active dark>
+      <v-tab class="ml-1" to="/calculator">Калькулятор</v-tab>
+      <v-tab to="/base-to-base">Системы счисления</v-tab>
+      <v-tab to="/weather">Погода</v-tab>
+      <v-spacer></v-spacer>
+      <v-tab>Обо мне</v-tab>
+    </v-tabs>
+  </v-card>
+</template>
