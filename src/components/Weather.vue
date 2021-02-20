a195df4033c8d128dc976ec06a4a205afd668e72
Дмитрий Халин <dimaskhln@gmail.com>
2021-02-20 13:31:16 +0700

    Added axios, weather uploading for three cities

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,61 +1,44 @@
 <template>
-  <v-container fluid
-    ><v-card color="blue-grey lighten-5" max-width="450" class="mx-auto my-12 pa-6">
-      <v-row>
-        <v-col>
-          <v-text-field v-model="number" label="Enter number" outlined></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row align="center">
-        <v-col class="d-flex" cols="12" sm="6">
-          <v-select v-model="fromBase" :items="bases" label="From" outlined></v-select>
-        </v-col>
-        <v-col class="d-flex" cols="12" sm="6">
-          <v-select v-model="toBase" :items="bases" label="To" outlined></v-select>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-btn class="mx-3 mb-3" color="green accent-2" v-on:click="convert()">Convert</v-btn>
-      </v-row>
-      <v-row
-        ><v-card class="mx-3 mb-3 pa-3" elevation="2" outlined>Answer: {{ result }}</v-card></v-row
-      >
+  <v-container fluid>
+    <v-card v-for="city in cities" :key="city.name" class="mx-auto my-12" max-width="374">
+      <v-card-title>{{ city.ruName }}</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          {{ city.weather }}
+        </v-row>
+      </v-card-text>
+    </v-card>
+    <v-card class="mx-auto my-12" max-width="374">
+      <v-card-title><v-icon>mdi-plus</v-icon> Добавить город</v-card-title>
     </v-card>
   </v-container>
 </template>
 
 <script>
+const apiKey = '5d498cca2f0e7342e2601295325ff976';
+
 export default {
   data() {
     return {
-      bases: [
-        { value: 2, text: 'Binary' },
-        { value: 8, text: 'Octal' },
-        { value: 10, text: 'Decimal' },
-        { value: 16, text: 'Hexadecimal' }
-      ],
-      fromBase: '',
-      toBase: '',
-      number: 0,
-      result: 0
+      cities: [
+        { name: 'Novosibirsk', ruName: '', weather: '' },
+        { name: 'Moscow', ruName: '', weather: '' },
+        { name: 'Sochi', ruName: '', weather: '' }
+      ]
     };
   },
   methods: {
-    convert: function() {
-      try {
-        if (this.fromBase === void 0) {
-          this.fromBase = 10;
-        }
-        if (this.toBase === void 0) {
-          this.toBase = 10;
-        }
-        this.result = parseInt(this.number.toString(), this.fromBase)
-          .toString(this.toBase)
-          .toUpperCase();
-      } catch (e) {
-        console.error('Error in convertNumber: ' + e);
-      }
+    getWeather: function() {
+      this.cities.forEach(cityObj => {
+        this.$axios
+          .get(`http://api.openweathermap.org/data/2.5/weather?q=${cityObj.name}&appid=${apiKey}&units=metric&lang=ru`)
+          .then(response => ((cityObj.weather = response.data.main.temp), (cityObj.ruName = response.data.name)));
+      });
     }
+  },
+  mounted() {
+    this.getWeather('Novosibirsk');
   }
 };
 </script>
