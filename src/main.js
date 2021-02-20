7753fbe16cb7d1aab5f6eb9562662d2440fa04f4
Дмитрий Халин <dimaskhln@gmail.com>
2021-02-20 13:31:16 +0700

    Added axios, weather uploading for three cities

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import VueAxios from './plugins/axios';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios);
 
 new Vue({
   vuetify,
