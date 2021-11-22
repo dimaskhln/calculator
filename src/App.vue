8974c8c43bf89e9b8ee0d3dc0ec92566a072550f
Халин Дмитрий <dm_ser_k@mail.ru>
2021-11-22 14:56:42 +0700

    Продолжаю с переводом

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
 //import Footer from './components/Footer.vue';
 
 import TabNavigator from './components/TabNavigator';
+import i18n from '@/plugins/i18n';
 export default {
   name: 'App',
   components: {
@@ -22,6 +23,11 @@ export default {
   },
   data() {
     return {};
+  },
+  methods: {
+    changeLocale(locale) {
+      i18n.locale = locale;
+    }
   }
 };
 </script>
--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -3,7 +3,7 @@ import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
 
-const messages = {
+export const messages = {
   ru: {
     HEAD_calculator: 'Калькулятор',
     HEAD_weather: 'Погода',
@@ -14,7 +14,7 @@ const messages = {
     CALC_lowSalary: 'Близко к моей желаемой зарплате, но хочется большего',
     CALC_okSalary: 'Да, такая зарплата меня устроит (на начальном этапе)',
     CALC_highSalary: 'Если это моя будущая зарплата, то считайте мы договорились',
-    CALC_superSalary: 'Пока можно начальство такими числами не пугать (ключевое слово "пока")',
+    CALC_superSalary: 'Пока можно начальство такими числами не пугать &#128517; (ключевое слово "пока")',
 
     WEATHER_dialogHead: 'Это не просто погодный виджет!',
     WEATHER_dialogBody: 'Да, погоду здесь узнать можно, но так же можно узнать в каких городах я уже побывал!',
@@ -42,68 +42,75 @@ const messages = {
     ABOUT_mainInfoBody:
       'Меня зовут Дмитрий.<br />Мне {{ age }} {{ ageWord }}. <br />Я web-разработчик. <br /> Мне нравится создавать удобные интерфейсы и наполнять приложения логикой.',
     ABOUT_hobbiesHead: 'Мои хобби',
-    ABOUT_hobbiesBody: '<a href="https://steamcommunity.com/id/dimastip" target="_blank">Компьютерные игры</a>, гитара, коллекционирование старых мобильных телефонов',
+    ABOUT_hobbiesBody:
+      '<a href="https://steamcommunity.com/id/dimastip" target="_blank">Компьютерные игры</a>, гитара, коллекционирование старых мобильных телефонов',
     ABOUT_achievementsHead: 'Мои достижения',
-    ABOUT_achievementsBody: 'За неполные три года работы в качестве Frontend-разработчика я !!!!!№!№" Web-приложение для управления охранными системами, доступное как на десктопе, так и на мобильных устройствах. При разработке использовались №%№;№;;№" HTML+CSS+JS, jQuery, бэкенд на C# (ASP.NET), MSSQL, Также одной из моих обязанностей была разработка и поддержка приложения-оболочки для этой системы управления на Android (Xamarin) с дополнительным функционалом.',
+    ABOUT_achievementsBody:
+      'За неполные три года работы в качестве Frontend-разработчика я !!!!!№!№" Web-приложение для управления охранными системами, доступное как на десктопе, так и на мобильных устройствах. При разработке использовались №%№;№;;№" HTML+CSS+JS, jQuery, бэкенд на C# (ASP.NET), MSSQL, Также одной из моих обязанностей была разработка и поддержка приложения-оболочки для этой системы управления на Android (Xamarin) с дополнительным функционалом.',
     ABOUT_whyChangeHead: 'Чего я хочу',
-    ABOUT_whyChangeBody: 'До того как я пришел в компанию, в которой работаю сейчас, Web-разработкой я не занимался вообще. Знания о JavaScript и jQuery пришлось добывать "в бою", поэтому есть ощущения что в них могут быть пробелы. Поэтому в первую очередь я хочу актуализировать свои навыки. Есть ощущение, что вокруг все уже перемещаются на летающих машинах, а я всё ещё езжу на повозке, запряженной лошадьми. Для их восполнения я хочу попасть в профессиональную команду, в которой я смогу выровнять свои знания и получить новые. Также мне хочется видеть впереди перспективы - и профессиональные и финансовые. Безусловно, я понимаю, что для этого нужно усердно и качественно работать, к чему я готов и чего я хочу.',
+    ABOUT_whyChangeBody:
+      'До того как я пришел в компанию, в которой работаю сейчас, Web-разработкой я не занимался вообще. Знания о JavaScript и jQuery пришлось добывать "в бою", поэтому есть ощущения что в них могут быть пробелы. Поэтому в первую очередь я хочу актуализировать свои навыки. Есть ощущение, что вокруг все уже перемещаются на летающих машинах, а я всё ещё езжу на повозке, запряженной лошадьми. Для их восполнения я хочу попасть в профессиональную команду, в которой я смогу выровнять свои знания и получить новые. Также мне хочется видеть впереди перспективы - и профессиональные и финансовые. Безусловно, я понимаю, что для этого нужно усердно и качественно работать, к чему я готов и чего я хочу.',
     ABOUT_skillsHead: 'Что я уже умею',
-    ABOUT_skillsBody: '<v-card-text>HTML, CSS, JavaScript, jQuery, Vue, SQL, PHP, C#</v-card-text><v-card-text>R, Статистический анализ данных</v-card-text><v-card-text>Photoshop, Sony Vegas (MAGIX Vegas) </v-card-text>',
+    ABOUT_skillsBody:
+      '<v-card-text>HTML, CSS, JavaScript, jQuery, Vue, SQL, PHP, C#</v-card-text><v-card-text>R, Статистический анализ данных</v-card-text><v-card-text>Photoshop, Sony Vegas (MAGIX Vegas) </v-card-text>',
     ABOUT_goalsHead: 'Чему хочу научиться',
     ABOUT_goalsBody: 'React',
-    ABOUT_footerContact: 'Свяжитесь со мной:',
-    ABOUT_footerIcons: 'Изображения сделаны <a href="https://www.freepik.com" title="Freepik">Freepik</a> из <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>'
+    ABOUT_footerContact: 'Свяжитесь со мной!',
+    ABOUT_footerIcons:
+      'Изображения сделаны <a href="https://www.freepik.com" title="Freepik">Freepik</a> из <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>'
   },
   en: {
-    HEAD_calculator: '',
-    HEAD_weather: '',
-    HEAD_todo: '',
-    HEAD_about: '',
+    HEAD_calculator: 'Calculator',
+    HEAD_weather: 'Weather',
+    HEAD_todo: 'ToDo',
+    HEAD_about: 'About me',
 
-    CALC_lowSalary: '',
+    CALC_lowSalary: "That's couldn't be my salary. Please try again.",
     CALC_okSalary: '',
     CALC_highSalary: '',
-    CALC_superSalary: '',
+    CALC_superSalary: "Wow! Don't frighten your boss with this numbers! &#128517; For now...",
 
-    WEATHER_dialogHead: '',
-    WEATHER_dialogBody: '',
-    WEATHER_wind: '',
-    WEATHER_addCity: '',
-    WEATHER_cityNamePlaceholder: '',
-    WEATHER_cityAddButton: '',
-    WEATHER_cityCancelButton: '',
-    WEATHER_footer: '',
-    WEATHER_deleteButton: '',
+    WEATHER_dialogHead: 'This is not just a weather widget!',
+    WEATHER_dialogBody: "Yeah, you can check the weather here, but also you can see in which cities I've already been.",
+    WEATHER_wind: 'wind',
+    WEATHER_addCity: 'Add city',
+    WEATHER_cityNamePlaceholder: 'City name',
+    WEATHER_cityAddButton: 'Add',
+    WEATHER_cityCancelButton: 'Cancel',
+    WEATHER_footer: 'Weather info from',
+    WEATHER_deleteButton: 'Delete',
 
-    TODO_addHead: '',
-    TODO_namePlaceholder: '',
-    TODO_descriptionPlaceholder: '',
-    TODO_priorityPlaceholder: '',
-    TODO_priorityNormal: '',
-    TODO_priorityHigh: '',
-    TODO_prioritySuper: '',
+    TODO_addHead: 'Add ',
+    TODO_namePlaceholder: 'Name',
+    TODO_descriptionPlaceholder: 'Description',
+    TODO_priorityPlaceholder: 'Priority',
+    TODO_priorityNormal: 'Normal',
+    TODO_priorityHigh: 'High',
+    TODO_prioritySuper: 'Super high',
 
-    ABOUT_dialogHead: '',
-    ABOUT_dialogBody: '',
-    ABOUT_mainInfoHead: '',
+    ABOUT_dialogHead: 'Welcome!',
+    ABOUT_dialogBody: "I'm Dmitriy and glad to see you here! Go through all the tabs to know something about me!",
+    ABOUT_mainInfoHead: 'Good evening!',
     ABOUT_mainInfoBody: '',
-    ABOUT_hobbiesHead: '',
+    ABOUT_hobbiesHead: 'My hobbies',
     ABOUT_hobbiesBody: '',
-    ABOUT_achievementsHead: '',
+    ABOUT_achievementsHead: 'My achievements',
     ABOUT_achievementsBody: '',
-    ABOUT_whyChangeHead: '',
+    ABOUT_whyChangeHead: 'My priorities',
     ABOUT_whyChangeBody: '',
-    ABOUT_skillsHead: '',
+    ABOUT_skillsHead: 'My skills',
     ABOUT_skillsBody: '',
-    ABOUT_goalsHead: '',
+    ABOUT_goalsHead: 'My goals',
     ABOUT_goalsBody: '',
-    ABOUT_footerContact: '',
-    ABOUT_footerIcons: 'Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from<a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>'
+    ABOUT_footerContact: 'Contact me!',
+    ABOUT_footerIcons:
+      'Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from<a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>'
   }
 };
 
 const i18n = new VueI18n({
-  locale: 'ru', // set locale
-  fallbackLocale: 'en', // set fallback locale
+  locale: 'en', // set locale
+  fallbackLocale: 'ru', // set fallback locale
   messages // set locale messages
 });
+export default i18n;
