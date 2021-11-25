f913dd8fb3c8756049957baea527b08d64f30b55
Дмитрий Халин <dimaskhln@gmail.com>
2021-11-25 22:34:11 +0700

    Переделал обо мне

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,3 @@
-// Про меня, про квн про общественную жизнь про навыки про то что пробовал в статистику про фронтенд про андроид про игрухи // Тест??? Работу хочу сменить //
-потому что хочу развиваться дальше, а тут консервативно и к новому относятся с опаской, а я хочу пробовать использовать фреймворки, вот вью поковырял // первую
-карточку надо сделать типа имя, возраст, образование и всё-такое
 <template>
   <v-container fluid>
     <v-row
@@ -27,7 +24,7 @@
         </v-card></v-col
       >
     </v-row>
-    <v-row
+    <v-row class="mb-3"
       ><v-col cols="12" sm="12" md="6">
         <v-card class="mt-3 mx-3">
           <v-card-title>{{ $t('ABOUT_achievementsHead') }}</v-card-title>
@@ -42,34 +39,28 @@
         </v-card></v-col
       >
     </v-row>
-    <v-row>
-      <v-footer width="100%" absolute>
-        <v-row>
-          <v-col cols="12" sm="12" md="6" class="text-center"
-            >{{ $t('ABOUT_footerContact') }} <br />
-            <a target="_blank" href="https://vk.com/thisisdimas"
-              ><v-img class="mx-3 d-inline-flex" height="32" width="32" src="/images/social/vk_128.png"></v-img
-            ></a>
-            <a target="_blank" href="https://t.me/dimaskhln"
-              ><v-img class="mx-3 d-inline-flex" height="32" width="32" src="/images/social/telegram_128.png"></v-img
-            ></a>
-            <a target="_blank" href="https://instagram.com/this.is.dimas"
-              ><v-img class="mx-3 d-inline-flex" height="32" width="32" src="/images/social/instagram_128.png"></v-img
-            ></a>
-            <br />
-            <v-icon class=" d-inline-flex">mdi-at</v-icon
-            ><a
-              class=" d-inline-flex"
-              href="mailto:me@dimaskhln.ru?bcc=dm_ser_k@mail.ru&?subject=%D0%A1%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B5%20%D1%81%20%D1%81%D0%B0%D0%B9%D1%82%D0%B0-%D1%80%D0%B5%D0%B7%D1%8E%D0%BC%D0%B5"
-              >me@dimaskhln.ru</a
-            ></v-col
-          ><v-col cols="12" sm="12" md="6" class="text-center">
-            {{ $t('ABOUT_footerIcons1') }} <a href="https://www.freepik.com" title="Freepik">Freepik</a> {{ $t('ABOUT_footerIcons2') }}
-            <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></v-col
-          >
-        </v-row>
-      </v-footer></v-row
-    >
+    <v-footer app bottom fixed padless class="matte-bg">
+      <v-col class="text-center"
+        >{{ $t('ABOUT_footerContact') }} <br />
+        <a target="_blank" href="https://vk.com/thisisdimas"
+          ><v-img class="mx-3 d-inline-flex" height="32" width="32" src="/images/social/vk_128.png"></v-img
+        ></a>
+        <a target="_blank" href="https://t.me/dimaskhln"
+          ><v-img class="mx-3 d-inline-flex" height="32" width="32" src="/images/social/telegram_128.png"></v-img
+        ></a>
+        <a target="_blank" href="https://instagram.com/this.is.dimas"
+          ><v-img class="mx-3 d-inline-flex" height="32" width="32" src="/images/social/instagram_128.png"></v-img
+        ></a>
+        <br />
+        <v-icon class=" d-inline-flex">mdi-at</v-icon
+        ><a
+          class=" d-inline-flex"
+          href="mailto:me@dimaskhln.ru?bcc=dm_ser_k@mail.ru&?subject=%D0%A1%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B5%20%D1%81%20%D1%81%D0%B0%D0%B9%D1%82%D0%B0-%D1%80%D0%B5%D0%B7%D1%8E%D0%BC%D0%B5"
+          >me@dimaskhln.ru</a
+        ><br />{{ $t('ABOUT_footerIcons1') }} <a href="https://www.freepik.com" title="Freepik">Freepik</a> {{ $t('ABOUT_footerIcons2') }}
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></v-col
+      >
+    </v-footer>
 
     <v-dialog v-if="showDialog" v-model="dialog" max-width="500">
       <v-card>
